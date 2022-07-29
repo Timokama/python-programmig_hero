56a07218c7df12be92930c39f8a5cd3af6b89f2e
Timothy Kamau <kamautimothy635@gmail.com>
2022-07-29 12:08:02 +0300

    Script 1 2 3

---
+++ Script.py
@@ -0,0 +1,13 @@
+from sys import argv
+#read the WYSS section for how to run this
+Script,First,Second,Third = argv
+
+print("The Script: ",Script)
+print("First: ",First)
+print("Second:",Second)
+print("Third:",Third)
+
+print(argv)
+
+#for arg in argv:
+#print(arg)
